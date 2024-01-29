efe59a4b6f4d7a45a3025213ae60cf24d356159f
solacode-SC <synelden18@gmail.com>
2024-01-29 16:42:53 +0100

    rps Game

--- rps_Game/main.js
+++ rps_Game/main.js
@@ -2,10 +2,19 @@ let userCho;
 let computerCho;
 let result;
 
+let userPts = 3;
+let computerPts = 3;
+
 const computerDisplay = document.getElementById('computer_cho');
 const userDisplay = document.getElementById('user_cho');
 const resultDisplay = document.getElementById('result');
 const possiblecho = document.querySelectorAll('button');
+
+const userPoints = document.getElementById('uPoints');
+const computerPoints = document.getElementById('cPoints');
+
+computerPoints.innerHTML = computerPts;
+userPoints.innerHTML = userPts;
 
 possiblecho.forEach(possiblecho => possiblecho.addEventListener('click', (e) => {
     userCho = e.target.id;
@@ -33,6 +42,8 @@ function generateComputerCho()
     computerDisplay.innerHTML = computerCho;
 }
 
+
+
 function getResult()
 {
     if (computerCho === userCho)
@@ -42,26 +53,52 @@ function getResult()
     if (computerCho === 'Rock'  && userCho === "Paper")
     {
         result = 'you win!';
+        userPts++;
+        computerPts--;
     }
     if (computerCho === 'Rock'  && userCho === "Scissors")
     {
         result = 'you lose!';
+        userPts--;
+        computerPts++;
     }
     if (computerCho === 'Paper'  && userCho === "Rock")
     {
         result = 'you lose!';
+        userPts--;
+        computerPts++;
     }
     if (computerCho === 'Paper'  && userCho === "Scissors")
     {
         result = 'you win!';
+        userPts++;
+        computerPts--;
     }
     if (computerCho === 'Scissors'  && userCho === "Paper")
     {
         result = 'you lose!';
+        userPts--;
+        computerPts++;
     }
     if (computerCho === 'Scissors'  && userCho === "Rock")
     {
         result = 'you win!';
+        userPts++;
+        computerPts--;
     }
     resultDisplay.innerHTML = result;
+    computerPoints.innerHTML = computerPts;
+    userPoints.innerHTML = userPts;
+    winner(computerPts, userPts);
+}
+
+function winner(cPoints, uPoints)
+{
+    if (cPoints === 0 || uPoints === 0)
+    {
+        resultDisplay.innerHTML = 'Game Result';
+        computerPoints.innerHTML = 3;
+        userPoints.innerHTML = 3;
+    }
+
 }
